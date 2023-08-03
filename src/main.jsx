08d1c9fc6b19751924ee6d3387f82129c531a745
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-08-03 14:13:34 +0530

    Introduction to RRD6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return(
+    <BrowserRouter>
+      <Link to='/'> Home </Link>
+      <Link to='/about'> About </Link>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+  </BrowserRouter>
+  )
+}
+
+function Home() {
+  return (
+    <h2>Hey, This is Home</h2>
+  )
+}
+
+function About() {
+  return (
+    <h2>Hey, This is About</h2>
+  )
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
+ 

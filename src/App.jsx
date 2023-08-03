0c4b9af398acb3efabf2d78f8a4b690e76f9dc36
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-08-03 15:35:07 +0530

    UI:About and Home page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
   return (
-    <div>Hello World</div>
-  )
+    <BrowserRouter>
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import App from './App'
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  return(
-    <BrowserRouter>
-      <Link to='/'> Home </Link>
-      <Link to='/about'> About </Link>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-  </BrowserRouter>
-  )
-}
-
-function Home() {
-  return (
-    <h2>Hey, This is Home</h2>
-  )
-}
-
-function About() {
-  return (
-    <h2>Hey, This is About</h2>
-  )
-}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )

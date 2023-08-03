b5739511ff0d720187c33d3b7ffbe62ce5898d5f
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-08-03 17:35:12 +0530

    Nesting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Vans from './pages/Vans';
+import Vans from './pages/vans/Vans';
 
 import './server';
-import VanDetail from './pages/VanDetail';
+import VanDetail from './pages/vans/VanDetail';
+import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
+import Income from './pages/host/Income';
+import Reviews from './pages/host/Reviews';
+import Dashboard from './pages/host/Dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <header>
-      <Link className="site-logo" to="/">#VanLife</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
-    </header>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/vans" element={<Vans />} />
-      <Route path="/vans/:id" element={<VanDetail />} />
-    </Routes>
-  </BrowserRouter>
-)
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+
+          <Route path="/host" element={<HostLayout />} >
+            <Route path='/host' element={<Dashboard />} />
+            <Route path='/host/income' element={<Income />} />
+            <Route path='/host/reviews' element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
